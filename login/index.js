bc36bc2b6fb79ce3ea2e7d4f2d83c2b2b1fbda4b
hamizan024 <hamizanfr024@gmail.com>
2023-05-05 13:58:07 +0700

    make js API

---
+++ login/index.js
@@ -0,0 +1,26 @@
+const loginBtn = document.getElementById("login-btn");
+loginBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // menghentikan perilaku default dari form
+
+  const email = document.getElementById("exampleInputEmail1").value;
+  const password = document.getElementById("exampleInputPassword1").value;
+
+  // mengirimkan permintaan login ke API
+  fetch("https://64530ec3e9ac46cedf1c7892.mockapi.io", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert("Login Berhasil ges")
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
