fbf59af04deee0868c1c19940cf5b5c1d22f46f6
Amanah Nur Az-zahroh <20104071@ittelkom-pwt.ac.id>
2023-05-05 19:55:51 +0700

    revisi register

--- Register/register.js
+++ Register/register.js
@@ -1,12 +1,12 @@
 const loginBtn = document.getElementById("login-btn");
 loginBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // menghentikan perilaku default dari form
+  event.preventDefault(); 
 
   const username = document.getElementById("exampleInputUsername1").value;
   const email = document.getElementById("exampleInputEmail1").value;
   const password = document.getElementById("exampleInputPassword1").value;
 
-  // mengirimkan permintaan login ke API
+  // mengirimkan permintaan Regiser ke API
   fetch("https://64530ec3e9ac46cedf1c7892.mockapi.io", {
     method: "POST",
     headers: {
