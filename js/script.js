8a9081c988040cb42482e1124d70a7cda2394307
hamizan024 <hamizanfr024@gmail.com>
2023-05-06 14:48:16 +0700

    Update JS

--- js/script.js
+++ js/script.js
@@ -85,4 +85,38 @@ function closeModal() {
   modal.style.display = "none";
 }
 
+/*
+const complaintBtn = document.getElementById("myButton");
+complaintBtn.addEventListener("click", (event) => {
+  event.preventDefault();
 
+  const name = document.getElementById("nama").value;
+  const email = document.getElementById("email").value;
+  const hp = document.getElementById("telepon").value;
+  const address = document.getElementById("lokasi").value;
+  const code = document.getElementById("zip-code").value;
+  const complaint = document.getElementById("keluhan").value;
+
+  // mengirimkan permintaan Regiser ke API
+  fetch("https://64530ec3e9ac46cedf1c7892.mockapi.io/users/:id/complaint", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      telepon: hp,
+      lokasi: address,
+      zipCode: code,
+      keluhan: complaint,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert("Laporan Berhasil!");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}); */
